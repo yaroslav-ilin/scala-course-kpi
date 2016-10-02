b8dc7c9c5e9c0deb9a5ec1a3fb038b5267dfda1f
Yaroslav Ilin <nilfalse@yandex.com>
2016-10-02 05:14:37 +0300

    task #7

---
+++ problem-set-1/task7.scala
@@ -0,0 +1,10 @@
+def last(list: List[Int]): Int = list match {
+  case Nil => throw new IllegalArgumentException
+  case x :: Nil => x
+  case _ => last(list.tail)
+}
+
+val list = List(1, 1, 2, 3, 5, 8)
+println(list + ".last  => " + list.last)
+println("last(" + list + ") => " + last(list))
+
